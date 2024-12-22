6fc983b2f92ebabfe4b0188fe11a96f2106c38c6
FloudMe77 <marecik.dariusz@gmail.com>
2024-12-22 22:53:52 +0100

    minimal refactor

--- code/kd-tree.py
+++ code/kd-tree.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import math
 class Point:
     def __init__(self,cords,number=None):
+        # number only for debug
         self.cords = cords
         self.amount_of_dimensions = len(self.cords)
         self.number = number
@@ -37,12 +38,12 @@ class Rectangle:
 
     def is_intersect(self,other):
         
-        p1, p2 = self.lower_left.cords, self.upper_right.cords
-        q1, q2 = other.lower_left.cords, other.upper_right.cords
+        lower_left_1_cords, upper_right_1_cords = self.lower_left.cords, self.upper_right.cords
+        lower_left_2_cords, upper_right_2_cords = other.lower_left.cords, other.upper_right.cords
 
         # Sprawdź brak przecięcia dla każdego wymiaru
-        for i in range(len(p1)):  # Zakładamy, że p1, p2, q1, q2 mają ten sam wymiar
-            if p2[i] < q1[i] or q2[i] < p1[i]:
+        for i in range(len(lower_left_1_cords)):  # Zakładamy, że lower_left_1_cords, upper_right_1_cords, lower_left_2_cords, upper_right_2_cords mają ten sam wymiar
+            if upper_right_1_cords[i] < lower_left_2_cords[i] or upper_right_2_cords[i] < lower_left_1_cords[i]:
                 return False  # Prostokąty się nie przecinają
         return True  # Prostokąty się przecinają
     
@@ -50,25 +51,25 @@ class Rectangle:
         """
         Sprawdza, czy other zawiera się w self.
         """
-        # R2 = (q1, q2) to prostokąt `other`
-        # R1 = (p1, p2) to prostokąt `self`
-        p1, p2 = self.lower_left.cords, self.upper_right.cords
-        q1, q2 = other.lower_left.cords, other.upper_right.cords
+        # R2 = (lower_left_2_cords, upper_right_2_cords) to prostokąt `other`
+        # R1 = (lower_left_1_cords, upper_right_1_cords) to prostokąt `self`
+        lower_left_1_cords, upper_right_1_cords = self.lower_left.cords, self.upper_right.cords
+        lower_left_2_cords, upper_right_2_cords = other.lower_left.cords, other.upper_right.cords
 
         # Sprawdź zawieranie dla każdego wymiaru
-        for i in range(len(p1)):  # Zakładamy, że p1, p2, q1, q2 mają tę samą długość
-            if not (p1[i] <= q1[i] <= q2[i] <= p2[i]):
+        for i in range(len(lower_left_1_cords)):  # Zakładamy, że lower_left_1_cords, upper_right_1_cords, lower_left_2_cords, upper_right_2_cords mają tę samą długość
+            if not (lower_left_1_cords[i] <= lower_left_2_cords[i] <= upper_right_2_cords[i] <= upper_right_1_cords[i]):
                 return False  # R2 nie jest w całości w R1
         return True  # R2 jest w całości w R1
     
     def intersection(self, other):
-        p1, p2 = self.lower_left.cords, self.upper_right.cords
-        q1, q2 = other.lower_left.cords, other.upper_right.cords
-        ll = deepcopy(p1)
-        ur = deepcopy(p2)
-        for i in range(len(p1)):  # Zakładamy, że p1, p2, q1, q2 mają tę samą długość
-            ll[i] = max(ll[i],p1[i],q1[i])
-            ur[i] = min(ur[i],p2[i],q2[i])
+        lower_left_1_cords, upper_right_1_cords = self.lower_left.cords, self.upper_right.cords
+        lower_left_2_cords, upper_right_2_cords = other.lower_left.cords, other.upper_right.cords
+        ll = deepcopy(lower_left_1_cords)
+        ur = deepcopy(upper_right_1_cords)
+        for i in range(len(lower_left_1_cords)):  # Zakładamy, że lower_left_1_cords, upper_right_1_cords, lower_left_2_cords, upper_right_2_cords mają tę samą długość
+            ll[i] = max(ll[i],lower_left_1_cords[i],lower_left_2_cords[i])
+            ur[i] = min(ur[i],upper_right_1_cords[i],upper_right_2_cords[i])
         return Rectangle(Point(ll),Point(ur))  # R2 jest w całości w R1
     
     def points_in_rectangle(self,points):
@@ -101,10 +102,12 @@ class KdTreeNode:
     def build(self):
         if len(self.points)==1: return
         self.points.sort(key = lambda x: x.cords[self.depth])
-        print(self.points[math.ceil(len(self.points)/2)-1].cords[self.depth])
-        left_rec, right_rec = self.devide_by_two(self.rectangle, (self.depth)%self.amount_of_dimensions, self.points[math.ceil(len(self.points)/2)-1].cords[(self.depth)%self.amount_of_dimensions])
-        self.left = KdTreeNode(self.points[0:math.ceil(len(self.points)/2)],self.amount_of_dimensions, (self.depth+1)%self.amount_of_dimensions, left_rec)
-        self.right = KdTreeNode(self.points[math.ceil(len(self.points)/2):],self.amount_of_dimensions, (self.depth+1)%self.amount_of_dimensions, right_rec )
+        median = math.ceil(len(self.points)/2)
+        axes = self.points[median-1].cords[self.depth%self.amount_of_dimensions]
+        print(axes)
+        left_rec, right_rec = self.devide_on_half_rectangle(self.rectangle, (self.depth)%self.amount_of_dimensions, axes)
+        self.left = KdTreeNode(self.points[0:median], self.amount_of_dimensions, (self.depth+1)%self.amount_of_dimensions, left_rec )
+        self.right = KdTreeNode(self.points[median:], self.amount_of_dimensions, (self.depth+1)%self.amount_of_dimensions, right_rec )
     
     def print_tree(self):
         print(self.points,self.depth, self.rectangle)
@@ -113,21 +116,24 @@ class KdTreeNode:
         if self.right:
             self.right.print_tree()
 
-    def devide_by_two(self,rec, amount_of_dimensions, division_line):
-        ll1 = rec.lower_left
-        ur2 = rec.upper_right
+    # możliwe, że powinna zostać przeniesiona gdzieś indziej
+    def devide_on_half_rectangle(self,rec, dimension_numer, axes):
+        # jeden to dolny, czy tam po lewej
+        # drugi to górny czy tam po prawej
+        lower_left_1 = rec.lower_left
+        upper_right_2 = rec.upper_right
 
-        ur1_cords = deepcopy(rec.upper_right.cords)
-        ur1_cords[amount_of_dimensions] = division_line
-        ur1 = Point(ur1_cords)
+        upper_right_1_cords = deepcopy(rec.upper_right.cords)
+        upper_right_1_cords[dimension_numer] = axes
+        upper_right_1 = Point(upper_right_1_cords)
 
-        ll2_cords = deepcopy(rec.lower_left.cords)
-        ll2_cords[amount_of_dimensions] = division_line
-        ll2 = Point(ll2_cords)
+        lower_left_2_cords = deepcopy(rec.lower_left.cords)
+        lower_left_2_cords[dimension_numer] = axes
+        lower_left_2 = Point(lower_left_2_cords)
         
-        return Rectangle(ll1,ur1), Rectangle(ll2,ur2)
+        return Rectangle(lower_left_1,upper_right_1), Rectangle(lower_left_2,upper_right_2)
     
-    def search(self,region):
+    def search_in_recangle(self,region):
         
         if self.left is None and self.right is None: # jesteśmy w liściu
             # print(self.points)
@@ -137,27 +143,26 @@ class KdTreeNode:
             print(self.points)
             return self.points
         if region.is_intersect(self.rectangle):
-            return self.left.search(region) + self.right.search(region)
+            return self.left.search_in_recangle(region) + self.right.search_in_recangle(region)
         return []
     
 class KdTree:
-    def __init__(self, points, amount_of_dimensions, begining_depth=0):
+    def __init__(self, points, amount_of_dimensions, begining_axis=0):
         points = [Point(point,e+1) for e,point in enumerate(points)]
-        print(points)
-        self.begining_depth = begining_depth
-        self.root = KdTreeNode(points,amount_of_dimensions,begining_depth,Rectangle(None,None,points))
+        self.begining_axis = begining_axis
+        self.root = KdTreeNode(points,amount_of_dimensions,begining_axis,Rectangle(None,None,points))
         self.amount_of_dimensions = amount_of_dimensions
         self.points = points
         
-    def search(self, region):
+    def search_in_recangle(self, region):
         print(region)
         region = self.root.rectangle.intersection(region)
-        return self.root.search(region)
+        return self.root.search_in_recangle(region)
 
     
 test = [(-5,1.5),(-3,4),(-2.5,1),(-5,7),(-2,6),(5,0),(0,3),(7,1),(2,7),(3,5)]
 a = KdTree(test,2)
 print(a.root.print_tree())
 
-print(a.search(Rectangle(Point((-3,0)),Point((10,10)))))
+print(a.search_in_recangle(Rectangle(Point((-3,0)),Point((10,10)))))
 # print([12,32]+[53,1])
